0bc39544815da61fce483c1e0415ded317ef5005
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-05-27 14:51:43 +0100

    Create products table

---
+++ db/create_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+def create_db():
+    conn = sqlite3.connect('db/amazon_prices.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            category TEXT NOT NULL,
+            price REAL NOT NULL,
+            created_at TEXT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    create_db()
