d92904072b7c5ba415ee421cf4c76f36910e1c59
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-05-27 17:07:40 +0100

    Scrape all search pages

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,6 +4,7 @@ import sqlite3
 from datetime import datetime
 import os
 
+
 def get_products(url, category):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -11,21 +12,23 @@ def get_products(url, category):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     products = soup.find_all('div', {'data-component-type': 's-search-result'})
-    
+
     data = []
     for product in products:
-        title_tag = product.find('h2')
+        title_tag = product.find(
+            'h2', {'class': 'a-size-mini a-spacing-none a-color-base s-line-clamp-2'})
 
         # Skip products without a title
         if title_tag is None:
-            continue 
+            continue
 
         # Skip products without a link
-        a_tag = title_tag.find('a')
+        a_tag = title_tag.find(
+            'a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'})
         if a_tag is None:
-            continue 
+            continue
         title = a_tag.text.strip()
-        url = a_tag['href']
+        url = 'https://www.amazon.com' + a_tag['href']
 
         price_tag = product.find('span', {'class': 'a-offscreen'})
         if price_tag is None:
@@ -37,27 +40,33 @@ def get_products(url, category):
 
     return data
 
-urls= {
+
+urls = {
     'CPU': 'https://www.amazon.com/s?k=cpu',
     'RAM': 'https://www.amazon.com/s?k=ram',
     'GPU': 'https://www.amazon.com/s?k=gpu'
 }
 
 
-
 def scrape():
-    for category, url in urls.items():
-        products_data = get_products(url, category)
+    for category, base_url in urls.items():
+        for page in range(1, 21):  # Change the range as needed
+            url = f"{base_url}&page={page}"
+            products_data = get_products(url, category)
 
-        # Check if data directory exists, if not, create it
-        if not os.path.exists('data'):
-            os.makedirs('data')
+            # Check if data directory exists, if not, create it
+            if not os.path.exists('data'):
+                os.makedirs('data')
 
-        # Save data to csv file
-        with open(f'data/{category}.csv', 'w') as f:
-            f.write('name,url,price,created_at\n')
-            for product in products_data:
-                f.write(','.join(map(str, product)) + '\n')  # Ensure all elements are strings
+            # Append data to csv file
+            file_path = f'data/{category}.csv'
+            file_exists = os.path.exists(file_path)
+            with open(file_path, 'a', encoding='utf-8') as f:
+                if not file_exists:
+                    f.write('name,url,price,created_at\n')
+                for product in products_data:
+                    f.write(','.join(map(str, product)) + '\n')
+
 
 if __name__ == "__main__":
     scrape()
