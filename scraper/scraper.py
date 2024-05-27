d156cb270e2fccff649b177776715108bbec8335
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-05-27 15:36:36 +0100

    First data

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 from datetime import datetime
+import os
 
 def get_products(url, category):
     headers = {
@@ -14,13 +15,15 @@ def get_products(url, category):
     data = []
     for product in products:
         title_tag = product.find('h2')
+
+        # Skip products without a title
         if title_tag is None:
             continue 
 
+        # Skip products without a link
         a_tag = title_tag.find('a')
         if a_tag is None:
             continue 
-
         title = a_tag.text.strip()
         url = a_tag['href']
 
@@ -33,3 +36,28 @@ def get_products(url, category):
         data.append((title, url, price, str(datetime.now())))
 
     return data
+
+urls= {
+    'CPU': 'https://www.amazon.com/s?k=cpu',
+    'RAM': 'https://www.amazon.com/s?k=ram',
+    'GPU': 'https://www.amazon.com/s?k=gpu'
+}
+
+
+
+def scrape():
+    for category, url in urls.items():
+        products_data = get_products(url, category)
+
+        # Check if data directory exists, if not, create it
+        if not os.path.exists('data'):
+            os.makedirs('data')
+
+        # Save data to csv file
+        with open(f'data/{category}.csv', 'w') as f:
+            f.write('name,url,price,created_at\n')
+            for product in products_data:
+                f.write(','.join(map(str, product)) + '\n')  # Ensure all elements are strings
+
+if __name__ == "__main__":
+    scrape()
